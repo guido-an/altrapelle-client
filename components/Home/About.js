bd9710a3b35f7e52ef51aaee501c5198c267bfae
guido-an <carucciguido@gmail.com>
2022-04-12 16:36:58 -0100

    fix broken link home

--- components/Home/About.js
+++ components/Home/About.js
@@ -1,39 +1,35 @@
-import styled from 'styled-components'
-import Button from '../atoms/Button'
-import OverTitleSection from '../atoms/OverTitleSection'
-import TitleSection from '../atoms/TitleSection'
+import styled from 'styled-components';
+import Button from '../atoms/Button';
+import OverTitleSection from '../atoms/OverTitleSection';
+import TitleSection from '../atoms/TitleSection';
 
-export default function About () {
+export default function About() {
   return (
     <Container>
-      <OverTitleSection align='center'>
-          IL NOSTRO MODO DI PRENDERCI CURA DI TE
-      </OverTitleSection>
-      <TitleSection align='center'>
-              Dermatecnologie
-      </TitleSection>
-      <P>Altrapelle è sinonimo di formulazioni dermatologicamente innovative e testate al fine
-di garantire efficacia e risultati davvero sorprendenti. Soluzioni efficaci adatte alle
-esigenze quotidiane e specifiche di ogni tipo di pelle e non solo.
+      <OverTitleSection align="center">IL NOSTRO MODO DI PRENDERCI CURA DI TE</OverTitleSection>
+      <TitleSection align="center">Dermatecnologie</TitleSection>
+      <P>
+        Altrapelle è sinonimo di formulazioni dermatologicamente innovative e testate al fine di
+        garantire efficacia e risultati davvero sorprendenti. Soluzioni efficaci adatte alle
+        esigenze quotidiane e specifiche di ogni tipo di pelle e non solo.
       </P>
-      <Button margin='40px auto 0' href='/altrapelle/tenless'>
-              Scopri le nostre linee
+      <Button margin="40px auto 0" href="/tenless">
+        Scopri le nostre linee
       </Button>
     </Container>
-
-  )
+  );
 }
 
 const Container = styled.div`
-    padding: 0 ${({ theme }) => theme.mobileContainer};
-`
+  padding: 0 ${({ theme }) => theme.mobileContainer};
+`;
 
 const P = styled.p`
-    text-align: center;
-    position: relative;
-    bottom: 20px;
-    @media(min-width: 768px){
-        max-width: 50%;
-        margin: 0 auto;
-    }
-`
+  text-align: center;
+  position: relative;
+  bottom: 20px;
+  @media (min-width: 768px) {
+    max-width: 50%;
+    margin: 0 auto;
+  }
+`;
