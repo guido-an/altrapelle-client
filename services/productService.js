2fe64e49246c8ccc44231b7d607c471992dad347
guido-an <carucciguido@gmail.com>
2022-05-04 10:14:36 -0100

    trigger deploy

--- services/productService.js
+++ services/productService.js
@@ -1,17 +1,15 @@
 class productService {
+  getAllProducts = async () => {
+    const res = await fetch(`${process.env.API_URL}/product/get-all`);
+    const data = await res.json();
+    return data;
+  };
 
-   getAllProducts = async () => {
-       const res =  await fetch(`${process.env.API_URL}/product/get-all`)
-       const data = await res.json()
-       return data
-   } 
-
-   getSingleProduct = async slug => {
-       const res = await fetch(`${process.env.API_URL}/product/get-single/${slug}`)
-       const data = await res.json()
-       return data
-   }
-
+  getSingleProduct = async slug => {
+    const res = await fetch(`${process.env.API_URL}/product/get-single/${slug}`);
+    const data = await res.json();
+    return data;
+  };
 }
 
-export default productService
+export default productService;
