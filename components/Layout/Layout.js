a9586d7d574cd331c6b7bff8892f3ad6eb6f9444
guido-an <carucciguido@gmail.com>
2022-08-08 17:11:16 +0200

    feat: add banner

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,12 +1,31 @@
-import Header from './Header'
-import Footer from './Footer'
+import Header from './Header';
+import Footer from './Footer';
+import styled from 'styled-components';
 
 export default function Layout({ children }) {
   return (
     <>
+      <Banner>
+        <p>
+          Saremo <strong>chiusi per ferie dal 5 al 22 agosto</strong>. Le spedizioni ripartiranno
+          quindi dal 23 Agosto. Vi ringraziamo e vi auguriamo buone vacanze!
+        </p>
+      </Banner>
       <Header />
-         <main>{children}</main>
+      <main>{children}</main>
       <Footer />
     </>
-  )
+  );
 }
+const Banner = styled.div`
+  background-color: ${({ theme }) => theme.colors.blue};
+  position: relative;
+  padding: 10px;
+  bottom: 10px;
+  p {
+    color: #fff;
+    text-align: center;
+    font-size: 14px;
+    padding: 0;
+  }
+`;
